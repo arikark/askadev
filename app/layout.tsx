3c0ecafd7ff8e53bb15f2c7895a6874dd2dd58da
arielkark <ariel@breezeai.com>
2025-01-02 09:12:20 +0200

    update layout and page components for improved SEO and responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,22 +19,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <meta property="og:image" content="/opengraph-image.png" />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="1280" />
-      <meta property="og:image:height" content="832" />
       <meta
         property="og:site_name"
-        content="Next.js + Notion — Waitlist Template"
+        content="Ask a dev - A community of trusted developers"
+      />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
       />
       <meta
         property="og:url"
         content="https://nextjs-notion-waitlist.vercel.app/"
       />
-      <meta name="twitter:image" content="/twitter-image.png" />
-      <meta name="twitter:image:type" content="image/png" />
-      <meta name="twitter:image:width" content="1280" />
-      <meta name="twitter:image:height" content="832" />
       <body className={FigtreeFont.className}>
         {children}
         <Toaster richColors position="top-center" />
--- app/page.tsx
+++ app/page.tsx
@@ -109,7 +109,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center overflow-x-clip pt-12 md:pt-24">
+    <main className="flex min-h-dvh flex-col items-center justify-center overflow-x-clip pt-12">
       <section className="flex flex-col items-center px-4 sm:px-6 lg:px-8">
         <CTA />
 
